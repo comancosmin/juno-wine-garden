b5682b8bd69c763b2123a80e616d95af62d410db
Cosmin <xcosmin53@gmail.com>
2023-07-06 03:50:21 +0300

    update ui

--- app/components/SecondBarTabs.jsx
+++ app/components/SecondBarTabs.jsx
@@ -19,7 +19,7 @@ function SecondWineTabs() {
     <>
       <Tab.Group>
         <Arrows />
-        <Tab.List className="flex gap-7 py-4 lg:text-xl  bottom-11 fixed w-screen  overflow-scroll px-10 whitespace-nowrap z-20 scrollbar-css lg:left-72">
+        <Tab.List className="flex gap-7 py-4 lg:text-xl  bottom-11 fixed w-screen  overflow-scroll px-10 whitespace-nowrap z-20 scrollbar-css lg:left-1/3">
           <Tab as={Fragment}>
             {({ selected }) => (
               /* Use the `selected` state to conditionally style the selected tab. */
